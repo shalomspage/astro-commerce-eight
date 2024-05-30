ff12698980972f308df43d235549b85b95dcdee3
El-Brukevwe Shalom <shalomzpage@gmail.com>
2024-05-30 19:44:19 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const cartIcon = document.querySelector(".cart");
+const modalLink = document.querySelector(".modal");
+// let isModalOpen = false;
+
+document.addEventListener("click", () => {
+  modalLink.classList.toggle("none");
+});

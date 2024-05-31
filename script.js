0798ce442c789ecb91c79e9f6f5ae6122de2c332
El-Brukevwe Shalom <shalomzpage@gmail.com>
2024-05-30 21:25:07 -0400

    js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cartIcon = document.querySelector(".cart-icon");
 const modalLink = document.querySelector(".modal");
-// let isModalOpen = false;
+
 
 cartIcon.addEventListener("click", () => {
   modalLink.classList.toggle("none");
